5c259bbee8ffc7544e89c74ffb5cc6a815742268
wangcaowei <414162938@qq.com>
2017-11-30 11:44:05 +0800

    fixed bug

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,13 +1,25 @@
 <template>
   <div id="context-menu" v-if="show" :style="{top:y,left:x}">
-      <slot></slot>
+      <ul>
+          <li :class="{active:item===activeMenu}" v-for="item in menu" @click="changActiveMenu(item)">{{item}}</li>
+      </ul>
   </div>
 </template>
 <script>
 export default {
-  props: ["ContextMenuClick"],
+  /* 
+        change 菜单选中项变化的事件 返回选中的某一项
+        menu: 菜单的数组
+     */
+  props: {
+    menu: {
+      require: true,
+      type: Array
+    }
+  },
   data() {
     return {
+      activeMenu: "",
       parentNode: null,
       show: false,
       x: 0,
@@ -17,30 +29,34 @@ export default {
   methods: {
     initEvent() {
       window.addEventListener("contextmenu", e => {
-        e.preventDefault();
         // 点击的元素是否在当前组件对应的dom中
-        const isCurrentEl = !!this.closest(e.target, this.$el);
-        if (e.target === this.parentNode || isCurrentEl) {
+        const isCurrentEl = !!this.closest(e.target, this.$el),
+          isInsideParentNode = !!this.closest(e.target, this.parentNode);
+        if (isInsideParentNode || isCurrentEl) {
+          e.preventDefault();
           this.show = true;
           // 如果点击的是父元素 调整位置
           if (!isCurrentEl) {
-            this.x = e.offsetX + "px";
-            this.y = e.offsetY + "px";
+            this.x = e.clientX - this.parentNode.getBoundingClientRect().left + "px";
+            this.y = e.clientY - this.parentNode.getBoundingClientRect().top + "px";
           }
         } else {
           this.show = false;
         }
       });
       window.addEventListener("click", e => {
-        event.stopPropagation();
+        // event.stopPropagation();
         // 点击的元素是否在当前组件对应的dom中
         const isCurrentEl = !!this.closest(e.target, this.$el);
         if (!isCurrentEl) {
           this.show = false;
         } else if (this.show) {
-          this.$emit("ContextMenuClick", e.target.textContent);
+          this.$emit("change", e.target.textContent);
         }
       });
+    },
+    changActiveMenu(item) {
+      this.activeMenu = item;
     },
     closest(el, $el) {
       while (el) {
@@ -59,9 +75,24 @@ export default {
   }
 };
 </script>
-<style>
+<style lang="scss">
 #context-menu {
+  background-color: #f6f6f6;
   box-shadow: 0 0 2px 2px #ccc;
   position: absolute;
+  ul {
+    list-style: none;
+    padding: 0;
+    li {
+      cursor: pointer;
+      width: 120px;
+      padding: 8px 0;
+      padding-left: 20px;
+    }
+  }
+  .active {
+    color: white;
+    background-color: #5bcd9a;
+  }
 }
 </style>
--- src/components/test.vue
+++ src/components/test.vue
@@ -1,75 +1,43 @@
 <template>
   <div class="test">
     <div class="app" v-for="item in test">
-      <ContextMenu v-on:ContextMenuClick="ContextMenuClick">
-        <slot>
-          <ul class="menu">
-            <li>菜单1</li>
-            <li>菜单2</li>
-            <li>菜单3</li>
-          </ul>
-        </slot>
-      </ContextMenu>
+      <ContextMenu :menu="menu" v-on:change="menuChange" />
     </div>
     <div class="app">
-      <ContextMenu v-on:ContextMenuClick="ContextMenuClick">
-        <slot>
-          <ul>
-            <li>菜单4</li>
-            <li>菜单5</li>
-            <li>菜单6</li>
-          </ul>
-        </slot>
-      </ContextMenu>
+      <ContextMenu :menu="menu" v-on:change="menuChange" />
     </div>
   </div>
 </template>
 
 <script>
-import ContextMenu from './menu'
+import ContextMenu from "./menu";
 export default {
-  name: 'hello',
-  data () {
+  name: "hello",
+  data() {
     return {
-      test:[1,2,3],
-    }
+      test: [1, 2, 3],
+      menu: ["menu1", "menu2", "menu3", "menu4", "menu5"]
+    };
   },
   methods: {
-    ContextMenuClick(val){
-      switch(val){
-        case '菜单1':
-        case '菜单2':
-        case '菜单3':
-        case '菜单4':
-        case '菜单5':
-        case '菜单6':
-            alert(val);
-            break;
-      }
+    menuChange(val) {
+      alert(val);
     }
   },
   components: {
     ContextMenu
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-  .app{
-    width: 200px;
-    height: 200px;
-    background-color: red;
-    margin-right: 100px;
-    display: inline-block;
-    position: relative;
-  }
-  .app ul{
-    list-style: none;
-    padding: 0;
-    margin: 0;
-  }
-  .menu{
-    width: 100px;
-  }
+.app {
+  width: 200px;
+  height: 200px;
+  background-color: red;
+  margin-right: 100px;
+  display: inline-block;
+  position: relative;
+}
 </style>
